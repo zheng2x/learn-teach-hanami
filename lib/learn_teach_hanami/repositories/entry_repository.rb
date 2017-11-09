a249e73d5c6b7d8c776027887db5f945ad8cc50c
zheng2x <ribbonitsky@naver.com>
2017-11-09 20:05:30 +0900

    Update entry_repository.rb

--- lib/learn_teach_hanami/repositories/entry_repository.rb
+++ lib/learn_teach_hanami/repositories/entry_repository.rb
@@ -2,4 +2,8 @@ class EntryRepository < Hanami::Repository
   def most_recent
     entries.order { created_at.desc }.call
   end
+  
+  def least_recent
+      entries.order{ created_at.desc }.call
+  end
 end
