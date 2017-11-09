2cd65b79d57710c7dfef14f7bfcce251881cec5c
zheng2x <ribbonitsky@naver.com>
2017-11-09 20:28:42 +0900

    if condition and sort thing in ruby

--- apps/web/controllers/entries/index.rb
+++ apps/web/controllers/entries/index.rb
@@ -3,7 +3,7 @@ module Web::Controllers::Entries
     include Web::Action
 
     expose :entries
-    expose :sort
+    expose :sort #not really necessary anymore
 
     def call(params)
       sort = params[:sort]
@@ -12,6 +12,10 @@ module Web::Controllers::Entries
       @entries = EntryRepository.new.most_recent
       @sort = sort
       # @sort = params[:sort] will be also fine
+      
+      # most_recent of least_recent
+      if @sort == 'most_recent'
+        @entries = EntryRepository.new.least_recent
     end
   end
 end
